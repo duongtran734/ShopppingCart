aff7c2cb2138fc16ddac3136189567375ca4baa2
duongtran734@gmail.com <duongtran734@gmail.com>
2021-08-18 22:51:49 -0400

    Seeded the Db

---
+++ Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopppingCart.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopppingCart.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            ////The compiler will dispose the object when done with using() statement
+            using (var context = new ShopppingCartDbContext
+                (serviceProvider.GetRequiredService<DbContextOptions<ShopppingCartDbContext>>()))
+            {
+                //check of DB is empty
+                if (context.Pages.Any())
+                {
+                    return;
+                }
+
+                //Seed data
+                context.Pages.AddRange(
+                    new Page
+                    {
+                        Title="Home",
+                        Slug="home",
+                        Content="home page",
+                        Sorting =0
+                    }, 
+                    new Page
+                    {
+                        Title = "About Us",
+                        Slug = "about-us",
+                        Content = "about page",
+                        Sorting = 100
+                    },
+                    new Page
+                    {
+                        Title = "Services",
+                        Slug = "services",
+                        Content = "services page",
+                        Sorting = 100
+                    }, 
+                    new Page
+                    {
+                        Title = "Contact",
+                        Slug = "contact",
+                        Content = "contact page",
+                        Sorting = 100
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ShopppingCart.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,25 @@ namespace ShopppingCart
 {
     public class Program
     {
+        //Entry Point of the Program
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
